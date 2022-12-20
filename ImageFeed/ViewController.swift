e65202236e569ed962d8f4103eea4d87dad1380f
Shliakhin Sergei <sshlyahin@yahoo.com>
2022-12-21 00:33:38 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by SERGEY SHLYAKHIN on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
