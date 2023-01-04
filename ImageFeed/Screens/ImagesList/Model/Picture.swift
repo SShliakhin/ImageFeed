5231991782bf9b9dc4ae885da9af1c62ce9a3a88
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-01-04 10:03:12 +0300

    fix: Change models

--- ImageFeed/Screens/ImagesList/Model/Picture.swift
+++ ImageFeed/Screens/ImagesList/Model/Picture.swift
@@ -5,13 +5,12 @@
 //  Created by SERGEY SHLYAKHIN on 22.12.2022.
 //
 
-import UIKit
+import Foundation
 
 struct Picture {
-    let asset: String
+    let image: String
     let date: Date
     let isFavorite: Bool
-    let height: CGFloat
 }
 
 // MARK: - MockData
@@ -19,23 +18,10 @@ extension Picture {
     static let pictures: [Picture] = {
         (0...20).map { number in
             Picture(
-                asset: String(describing: number),
+                image: String(describing: number),
                 date: Date(),
-                isFavorite: number % 2 == 0,
-                height: getHeight(
-                    for: UIImage(named: String(describing: number))
-                )
+                isFavorite: number % 2 == 0
             )
         }
     }()
-    
-    static func getHeight(for image: UIImage?) -> CGFloat {
-        guard let image = image else { return 0 }
-
-        let imageSize = image.size
-        let aspectRatio = imageSize.height / imageSize.width
-        let cellWidth = UIScreen.main.bounds.width - Theme.spacing(usage: .standard2) * 2
-        let cellHeight = cellWidth * aspectRatio + Theme.spacing(usage: .standardHalf) * 2
-        return cellHeight
-    }
 }
--- ImageFeed/Screens/ImagesList/Model/PictureViewModel.swift
+++ ImageFeed/Screens/ImagesList/Model/PictureViewModel.swift
@@ -8,7 +8,40 @@
 import UIKit
 
 struct PictureViewModel {
-    let image: UIImage
+    let image: UIImage?
     let dateString: String
     var isFavorite: Bool
+    let height: CGFloat
+}
+
+// MARK: MockData
+extension PictureViewModel {
+    static let pictureViewModels: [PictureViewModel] = {
+        let pictures = Picture.pictures
+        return pictures.map { item in
+                .init(
+                    image: UIImage(named: item.image),
+                    dateString: dateFormatter.string(from: item.date),
+                    isFavorite: item.isFavorite,
+                    height: getCellHeight(by: UIImage(named: item.image))
+                )
+        }
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    static func getCellHeight(by image: UIImage?) -> CGFloat {
+        guard let image = image else { return 0 }
+        
+        let imageSize = image.size
+        let aspectRatio = imageSize.height / imageSize.width
+        let cellWidth = UIScreen.main.bounds.width - Theme.spacing(usage: .standard2) * 2
+        let cellHeight = cellWidth * aspectRatio + Theme.spacing(usage: .standardHalf) * 2
+        return cellHeight
+    }
 }
