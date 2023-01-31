408f614242b414889aab9e15f2cbe04eef100fca
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-01-31 07:36:11 +0300

    feat: Add unsplash constants

---
+++ ImageFeed/Resources/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by SERGEY SHLYAKHIN on 31.01.2023.
+//
+
+import Foundation
+
+enum Constant: String {
+    case accessKey = "c8SI91LkiH6yY5SDWDKLtjNOYdgPaKAEwmD2dWs35eU"
+    case secretKey = "qg_ghS6ZMMDJP1aU1oiC6Bts3KTDKBt03G28yhrTMrM"
+    case redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    case accessScope = "public+read_user+write_likes"
+    case defaultBaseURL = "https://api.unsplash.com"
+}
+
+extension String {
+    static func key(_ constant: Constant) -> Self { constant.rawValue }
+}
+
+extension URL {
+    static var defaultBaseURL: Self { .init(string: .key(.defaultBaseURL))! }
+}
