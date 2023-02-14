47e30e139316b6a9c6b6ef32e2fa38b2150afd9a
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-02-14 18:26:53 +0300

    feat: Add handling for a potential race

--- ImageFeed/Modules/Auth/AuthInteractor.swift
+++ ImageFeed/Modules/Auth/AuthInteractor.swift
@@ -11,6 +11,9 @@ final class AuthInteractor: IAuthInteractorInput {
     weak var output: IAuthInteractorOutput?
     private var storage: ITokenStorage
     private let network: APIClient
+	
+	private var task: NetworkTask?
+	private var lastCode: String?
     
     init(storage: ITokenStorage, network: APIClient) {
         self.storage = storage
@@ -18,17 +21,24 @@ final class AuthInteractor: IAuthInteractorInput {
     }
     
     func fetchBearerTokenByCode(_ code: String) {
+		assert(Thread.isMainThread)
+		if lastCode == code { return }
+		task?.cancel()
+		lastCode = code
+		
         let resourse = UnsplashAPI.getAuthTokenRequest(code)
         let request = PostRequest(endpoint: resourse.url, body: "")
-        
-        network.send(request){ [weak self] (result: Result<OAuthTokenResponseBody, APIError>) in
+		
+		task = network.send(request){ [weak self] (result: Result<OAuthTokenResponseBody, APIError>) in
             guard let self = self else { return }
             switch result {
             case .success(let body):
                 self.storage.token = body.accessToken
                 self.output?.didFetchBearerTokenSuccess("Token: =========== \(body.accessToken)")
+				self.task = nil
             case .failure(let error):
                 self.output?.didFetchBearerTokenFailure(error: error)
+				self.lastCode = nil
             }
         }
     }
