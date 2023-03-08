886317f3c4baad7cdfbe11099a64d18a6dc65bc0
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-03-08 04:37:33 +0300

    style: Clean up library modules

--- ImageFeed/Library/Utils/UIStackView+Extensions.swift
+++ ImageFeed/Library/Utils/UIStackView+Extensions.swift
@@ -1,28 +1,21 @@
-//
-//  UIStackView+Extensions.swift
-//  ImageFeed
-//
-//  Created by SERGEY SHLYAKHIN on 12.01.2023.
-//
-
 import UIKit
 
 extension UIStackView {
-    func arrangeStackView(
-        subviews: [UIView],
-        spacing: CGFloat = 0,
-        axis: NSLayoutConstraint.Axis = .horizontal,
-        distribution: UIStackView.Distribution = .fill,
-        aligment: UIStackView.Alignment = .fill
-    ) {
-        self.axis = axis
-        self.spacing = spacing
-        self.distribution = distribution
-        self.alignment = aligment
-        
-        subviews.forEach { item in
-            item.translatesAutoresizingMaskIntoConstraints = false
-            self.addArrangedSubview(item)
-        }
-    }
+	func arrangeStackView(
+		subviews: [UIView],
+		spacing: CGFloat = 0,
+		axis: NSLayoutConstraint.Axis = .horizontal,
+		distribution: UIStackView.Distribution = .fill,
+		aligment: UIStackView.Alignment = .fill
+	) {
+		self.axis = axis
+		self.spacing = spacing
+		self.distribution = distribution
+		self.alignment = aligment
+		
+		subviews.forEach { item in
+			item.translatesAutoresizingMaskIntoConstraints = false
+			self.addArrangedSubview(item)
+		}
+	}
 }
