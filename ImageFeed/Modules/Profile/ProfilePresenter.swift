4427f9c4ab304307b1c7afe6ca98c3c345f18c65
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-03-08 05:47:35 +0300

    style: Clean up Profile module

--- ImageFeed/Modules/Profile/ProfilePresenter.swift
+++ ImageFeed/Modules/Profile/ProfilePresenter.swift
@@ -7,34 +7,37 @@
 
 import Foundation
 
-final class ProfilePresenter: IProfileViewOutput {
-    weak var view: IProfileViewInput?
-    private let interactor: IProfileInteractorInput
-    private let router: IProfileRouter
+final class ProfilePresenter {
+	weak var view: IProfileViewInput?
+	private let interactor: IProfileInteractorInput
+	private let router: IProfileRouter
 	private let profile: ProfileResult
-    
+	
 	init(interactor: IProfileInteractorInput, router: IProfileRouter, profile: ProfileResult) {
-        self.interactor = interactor
-        self.router = router
+		self.interactor = interactor
+		self.router = router
 		self.profile = profile
-    }
-    
+	}
+}
+
+// MARK: - IProfileViewOutput
+
+extension ProfilePresenter: IProfileViewOutput {
 	func viewDidLoad() {
 		view?.showProfile(profile: profile)
 	}
-    func didTapLogout() {
-        interactor.cleanUpStorage()
-    }
+	func didTapLogout() {
+		interactor.cleanUpStorage()
+	}
 }
 
 // MARK: - IProfileInteractorOutput
 
 extension ProfilePresenter: IProfileInteractorOutput {
-    func didCleanUpStorage() {
-        let emptyCode = ""
-        router.navigate(.toAuth(emptyCode))
-    }
-	
+	func didCleanUpStorage() {
+		let emptyCode = ""
+		router.navigate(.toAuth(emptyCode))
+	}
 	func didFetchProfileImageURL(_ url: URL) {
 		view?.updateAvatarURL(url)
 	}
--- ImageFeed/Modules/Profile/ProfileViewController.swift
+++ ImageFeed/Modules/Profile/ProfileViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 import Kingfisher
 
 final class ProfileViewController: UIViewController {
-
-    // MARK: - UI
+	private let presenter: IProfileViewOutput
+	
+	// MARK: - UI
 	private lazy var profileImageView: UIImageView = {
 		let imageView = UIImageView()
 		imageView.contentMode = .scaleAspectFill
@@ -20,39 +21,37 @@ final class ProfileViewController: UIViewController {
 		imageView.image = Theme.image(kind: .personPlaceholder)
 		return imageView
 	}()
-    
-    private lazy var nameLabel: UILabel = {
-        let label = UILabel()
-        label.font = Theme.font(style: .bold23)
-        label.textColor = Theme.color(usage: .ypWhite)
-        return label
-    }()
-    
-    private lazy var loginNameLabel: UILabel = {
-        let label = UILabel()
-        label.font = Theme.font(style: .regular13)
-        label.textColor = Theme.color(usage: .ypGray)
-        return label
-    }()
-    
-    private lazy var descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.font = Theme.font(style: .regular13)
-        label.textColor = Theme.color(usage: .ypWhite)
-        return label
-    }()
-    
-    private lazy var logoutButton: UIButton = {
-        let button = UIButton(type: .custom)
-        let image = Theme.image(kind: .exitIcon)
-        button.setBackgroundImage(image, for: .normal)
-        button.tintColor = Theme.color(usage: .ypRed)
-        return button
-    }()
+	
+	private lazy var nameLabel: UILabel = {
+		let label = UILabel()
+		label.font = Theme.font(style: .bold23)
+		label.textColor = Theme.color(usage: .ypWhite)
+		return label
+	}()
+	
+	private lazy var loginNameLabel: UILabel = {
+		let label = UILabel()
+		label.font = Theme.font(style: .regular13)
+		label.textColor = Theme.color(usage: .ypGray)
+		return label
+	}()
+	
+	private lazy var descriptionLabel: UILabel = {
+		let label = UILabel()
+		label.font = Theme.font(style: .regular13)
+		label.textColor = Theme.color(usage: .ypWhite)
+		return label
+	}()
+	
+	private lazy var logoutButton: UIButton = {
+		let button = UIButton(type: .custom)
+		let image = Theme.image(kind: .exitIcon)
+		button.setBackgroundImage(image, for: .normal)
+		button.tintColor = Theme.color(usage: .ypRed)
+		return button
+	}()
 	
 	// MARK: - Properties
-	private let presenter: IProfileViewOutput
-	
 	var profileViewModel: ProfileResult? {
 		didSet {
 			guard let profile = profileViewModel else { return }
@@ -61,35 +60,35 @@ final class ProfileViewController: UIViewController {
 			descriptionLabel.text = profile.bio
 		}
 	}
-    
-    // MARK: - Init
-    init(presenter: IProfileViewOutput) {
-        self.presenter = presenter
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setup()
-        applyStyle()
-        applyLayout()
-        
-        presenter.viewDidLoad()
-    }
+	
+	// MARK: - Init
+	init(presenter: IProfileViewOutput) {
+		self.presenter = presenter
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setup()
+		applyStyle()
+		applyLayout()
+		
+		presenter.viewDidLoad()
+	}
 }
 
 // MARK: - IProfileViewInput
 
 extension ProfileViewController: IProfileViewInput {
-    func showProfile(profile: ProfileResult) {
-        profileViewModel = profile
-    }
+	func showProfile(profile: ProfileResult) {
+		profileViewModel = profile
+	}
 	func updateAvatarURL(_ profileImageURL: URL) {
 		profileImageView.kf.indicatorType = .activity
 		profileImageView.kf.setImage(
@@ -108,53 +107,53 @@ private extension ProfileViewController {
 
 // MARK: - UIComponent
 private extension ProfileViewController {
-    func setup() {
-        logoutButton.addTarget(
-            self,
-            action: #selector(logoutButtonTapped),
-            for: .primaryActionTriggered
-        )
-    }
-    func applyStyle() {
-        view.backgroundColor = Theme.color(usage: .ypBlack)
-    }
-    func applyLayout() {
+	func setup() {
+		logoutButton.addTarget(
+			self,
+			action: #selector(logoutButtonTapped),
+			for: .primaryActionTriggered
+		)
+	}
+	func applyStyle() {
+		view.backgroundColor = Theme.color(usage: .ypBlack)
+	}
+	func applyLayout() {
 		let vStackView = UIStackView()
 		let hStackView = UIStackView()
 		
-        hStackView.arrangeStackView(
-            subviews: [
-                profileImageView,
-                UIView(),
-                logoutButton
-            ],
-            aligment: .center
-        )
-        
-        vStackView.arrangeStackView(
-            subviews: [
-                hStackView,
-                nameLabel,
-                loginNameLabel,
-                descriptionLabel
-            ],
+		hStackView.arrangeStackView(
+			subviews: [
+				profileImageView,
+				UIView(),
+				logoutButton
+			],
+			aligment: .center
+		)
+		
+		vStackView.arrangeStackView(
+			subviews: [
+				hStackView,
+				nameLabel,
+				loginNameLabel,
+				descriptionLabel
+			],
 			spacing: Theme.spacing(usage: .standard),
-            axis: .vertical,
-            aligment: .leading
-        )
-        
-        [vStackView].forEach { item in
-            item.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(item)
-        }
-        
-        NSLayoutConstraint.activate([
-            vStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Theme.spacing(usage: .standard4)),
-            vStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Theme.spacing(usage: .standard2)),
-            vStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Theme.spacing(usage: .standard2)),
-            profileImageView.heightAnchor.constraint(equalToConstant: Theme.size(kind: .profileImage)),
-            profileImageView.widthAnchor.constraint(equalToConstant: Theme.size(kind: .profileImage)),
-            hStackView.widthAnchor.constraint(equalTo: vStackView.widthAnchor, constant: -Theme.spacing(usage: .standard))
-        ])
-    }
+			axis: .vertical,
+			aligment: .leading
+		)
+		
+		[vStackView].forEach { item in
+			item.translatesAutoresizingMaskIntoConstraints = false
+			view.addSubview(item)
+		}
+		
+		NSLayoutConstraint.activate([
+			vStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Theme.spacing(usage: .standard4)),
+			vStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Theme.spacing(usage: .standard2)),
+			vStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Theme.spacing(usage: .standard2)),
+			profileImageView.heightAnchor.constraint(equalToConstant: Theme.size(kind: .profileImage)),
+			profileImageView.widthAnchor.constraint(equalToConstant: Theme.size(kind: .profileImage)),
+			hStackView.widthAnchor.constraint(equalTo: vStackView.widthAnchor, constant: -Theme.spacing(usage: .standard))
+		])
+	}
 }
