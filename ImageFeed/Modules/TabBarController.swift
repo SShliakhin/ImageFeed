54e527fd0fc51211edbdeaf2eac035b0e76bd826
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-03-08 05:48:59 +0300

    style: Clean up TabBarVC

--- ImageFeed/Modules/TabBarController.swift
+++ ImageFeed/Modules/TabBarController.swift
@@ -8,24 +8,24 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setupAppearance()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		setupAppearance()
+	}
 }
 
 private extension TabBarController {
-    func setupAppearance() {
-        let appearance = UITabBarAppearance()
-        appearance.backgroundColor = Theme.color(usage: .ypBlack)
-        
-        tabBar.standardAppearance = appearance
-        if #available(iOS 15.0, *) {
-            tabBar.scrollEdgeAppearance = appearance
-        }
-        
-        tabBar.tintColor = Theme.color(usage: .ypWhite)
-        tabBar.clipsToBounds = true
-    }
+	func setupAppearance() {
+		let appearance = UITabBarAppearance()
+		appearance.backgroundColor = Theme.color(usage: .ypBlack)
+		
+		tabBar.standardAppearance = appearance
+		if #available(iOS 15.0, *) {
+			tabBar.scrollEdgeAppearance = appearance
+		}
+		
+		tabBar.tintColor = Theme.color(usage: .ypWhite)
+		tabBar.clipsToBounds = true
+	}
 }
