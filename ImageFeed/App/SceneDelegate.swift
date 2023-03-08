8ab8691ec71d7e911d0ead9d6f7792f568ad35c0
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-03-08 04:19:36 +0300

    style: Clean up App modules

--- ImageFeed/App/SceneDelegate.swift
+++ ImageFeed/App/SceneDelegate.swift
@@ -8,26 +8,26 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
-    var container: DependencyContainer?
-
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        
-        let rootViewController = RootViewController()
-        container = DependencyContainer(rootVC: rootViewController)
-        rootViewController.factory = container
-        rootViewController.start()
-        
-        window.rootViewController = rootViewController
-        window.makeKeyAndVisible()
-        self.window = window
-
-    }
+	
+	var window: UIWindow?
+	var container: DependencyContainer?
+	
+	func scene(
+		_ scene: UIScene,
+		willConnectTo session: UISceneSession,
+		options connectionOptions: UIScene.ConnectionOptions
+	) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: windowScene)
+		
+		let rootViewController = RootViewController()
+		container = DependencyContainer(rootVC: rootViewController)
+		rootViewController.factory = container
+		rootViewController.start()
+		
+		window.rootViewController = rootViewController
+		window.makeKeyAndVisible()
+		self.window = window
+		
+	}
 }
